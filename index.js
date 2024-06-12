415c4d0410370cfa8d9aa1d32120cbd0e7f67c58
Md Alamin <mdalamincodes@gmail.com>
2024-06-13 03:48:53 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const TelegramBot = require("node-telegram-bot-api");
+const axios = require("axios");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+const token = "6991503389:AAFOuZgAWKhLjNJ4XP1w9PaSU6Y72uX0Bmw";
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const userInput = msg.text;
+  if (userInput === "Thank you".toLocaleLowerCase()) {
+    bot.sendMessage(
+      chatId,
+      `Welcome ${msg.chat.first_name} ${msg.chat.last_name}`
+    );
+    return;
+  }
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=b8c67f7acd80444ee2085a17b180b843`
+    );
+    const data = response.data;
+    const weather = data.weather[0].description;
+    const temperature = data.main.temp - 273.15;
+    const city = data.name;
+    const humidity = data.main.humidity;
+    const pressure = data.main.pressure;
+    const windSpeed = data.wind.speed;
+    const message = `The weather in ${city} is ${weather} with a temperature of ${temperature.toFixed(
+      2
+    )}°C. The humidity is ${humidity}%, the pressure is ${pressure}hPa, and the wind speed is ${windSpeed}m/s.`;
+
+    bot.sendMessage(chatId, message);
+  } catch (error) {
+    bot.sendMessage(chatId, "City doesn't exist.");
+  }
+});
