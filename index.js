4e47224fa97b0cabff5d9973d525960244a9c754
Md Alamin <mdalamincodes@gmail.com>
2024-06-15 04:25:11 +0600

    test webhook

--- index.js
+++ index.js
@@ -1,60 +1,35 @@
-const TelegramBot = require("node-telegram-bot-api");
-const axios = require("axios");
+require("dotenv").config();
 const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+
+const { TELEGRAM_TOKEN, SERVER_URL } = process.env;
+const TELEGRAM_API = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`;
+const URI = `/webhook/${TELEGRAM_TOKEN}`;
+const WEBHOOK_URL = SERVER_URL + URI;
+
 const app = express();
-const port = 5000;
+app.use(bodyParser.json());
 
-const token = "6991503389:AAFOuZgAWKhLjNJ4XP1w9PaSU6Y72uX0Bmw";
-const webhookUrl = `https://telegram-weather-check-bot.vercel.app/webhook/${token}`;
-const bot = new TelegramBot(token, { webHook: { port: port } });
+const init = async () => {
+  const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`);
+  console.log(res.data);
+};
 
-bot.setWebHook(webhookUrl);
+app.post(URI, async (req, res) => {
+  console.log(req.body);
 
-app.post(`/webhook/${token}`, async(req, res)=>{
-    await bot.processUpdate(req.body)
-    res.sendStatus(200)
-})
+  const chatId = req.body.message.chat.id;
+  const text = req.body.message.text;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+  await axios.post(`${TELEGRAM_API}/sendMessage`, {
+    chat_id: chatId,
+    text: text,
+  });
+  return res.send();
 });
 
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
-
-
-
-const bot = new TelegramBot(token, { polling: true });
-
-bot.on("message", async (msg) => {
-  const chatId = msg.chat.id;
-  const userInput = msg.text;
-  if (userInput === "Thank you".toLocaleLowerCase()) {
-    bot.sendMessage(
-      chatId,
-      `Welcome ${msg.chat.first_name} ${msg.chat.last_name}`
-    );
-    return;
-  }
-  try {
-    const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=b8c67f7acd80444ee2085a17b180b843`
-    );
-    const data = response.data;
-    const weather = data.weather[0].description;
-    const temperature = data.main.temp - 273.15;
-    const city = data.name;
-    const humidity = data.main.humidity;
-    const pressure = data.main.pressure;
-    const windSpeed = data.wind.speed;
-    const message = `The weather in ${city} is ${weather} with a temperature of ${temperature.toFixed(
-      2
-    )}°C. The humidity is ${humidity}%, the pressure is ${pressure}hPa, and the wind speed is ${windSpeed}m/s.`;
-
-    bot.sendMessage(chatId, message);
-  } catch (error) {
-    bot.sendMessage(chatId, "City doesn't exist.");
-  }
+app.listen(process.env.PORT || 5000, async () => {
+  console.log("🚀 app running on port", process.env.PORT || 5000);
+  await init();
 });
