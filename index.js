5c5241aeb36bb804ccd1347a30a56a161c317d88
Md Alamin <mdalamincodes@gmail.com>
2024-11-24 04:39:41 +0600

    change port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const port = 5000;
+const port = 5500;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
